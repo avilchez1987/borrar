bd6afdb6b3791cd06437cc38a400af4566aea7cf
Alfonso Vilchez <a.vilchez1987@gmail.com>
2024-08-02 22:05:57 -0500

    mas cambios

--- api/index.js
+++ api/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const serverless = require('serverless-http'); // Añade este módulo
 
 require('dotenv').config();
 
 const app = express();
-
 app.use(cors());
-
 app.use(express.json());
 
+// Define las rutas para tu API
 app.use('/api', require('../routes/user'));
 
-// Exporta la aplicación como una función handler
-module.exports.handler = serverless(app);
+// Exporta una función que maneje las solicitudes HTTP
+module.exports = (req, res) => {
+  // Maneja la solicitud con Express
+  app(req, res);
+};
